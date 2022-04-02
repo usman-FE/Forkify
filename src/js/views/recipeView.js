75aae918a5e46a6618f107b72d2cfc5361e81ef4
Usman-FE <ch.usman564@gmail.com>
2022-04-02 12:42:41 +0500

    fracty 2

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './view.js';
 
 class RecipeView extends View {
@@ -35,8 +35,9 @@ class RecipeView extends View {
   _generateMarkup() {
     return `
     <figure class="recipe__fig">
-          <img src="${this._data.image}" alt="${this._data.title
-      }" class="recipe__img" />
+          <img src="${this._data.image}" alt="${
+      this._data.title
+    }" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${this._data.title}</span>
           </h1>
@@ -47,27 +48,31 @@ class RecipeView extends View {
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime
-      }</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${
+              this._data.cookingTime
+            }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${this._data.servings
-      }</span>
+            <span class="recipe__info-data recipe__info-data--people">${
+              this._data.servings
+            }</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--upate-servings" data-update-to="${this._data.servings - 1
-      }">
+              <button class="btn--tiny btn--upate-servings" data-update-to="${
+                this._data.servings - 1
+              }">
                 <svg>
                   <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
-              <button class="btn--tiny btn--upate-servings" data-update-to="${this._data.servings + 1
-      }">
+              <button class="btn--tiny btn--upate-servings" data-update-to="${
+                this._data.servings + 1
+              }">
                 <svg>
                   <use href="${icons}#icon-plus-circle"></use>
                 </svg>
@@ -82,8 +87,9 @@ class RecipeView extends View {
           </div>
           <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : ''
-      }"></use>
+              <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
             </svg>
           </button>
         </div>
@@ -93,22 +99,23 @@ class RecipeView extends View {
           <ul class="recipe__ingredient-list">
 
           ${this._data.ingredients
-        .map((ing) => {
-          return `
+            .map((ing) => {
+              return `
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''
-            }</div>
+              <div class="recipe__quantity">${
+                ing.quantity ? fracty(ing.quantity).toString() : ''
+              }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
               </div>
             </li>
             `;
-        })
-        .join('')}
+            })
+            .join('')}
       
           </ul>
         </div>
@@ -117,8 +124,9 @@ class RecipeView extends View {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${this._data.publisher
-      }</span>. Please check out
+            <span class="recipe__publisher">${
+              this._data.publisher
+            }</span>. Please check out
             directions at their website.
           </p>
           <a
