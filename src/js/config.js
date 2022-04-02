5dcee968ff61fe437481997f0e488c6d8627e5df
Usman-FE <ch.usman564@gmail.com>
2022-04-02 13:03:37 +0500

    updated api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '4985dc83-f367-4e4b-9bc4-e06ef6fdb9a6';
+export const KEY = '9c8251c7-1b17-4f04-bb57-72973aae4475';
 export const MODAL_CLOSE_SEC = 2.5;
